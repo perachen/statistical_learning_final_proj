cdd74f969e39d30897f58e5f9377950836034af8
Perach Hen <64138377+perachen@users.noreply.github.com>
2022-04-01 20:57:50 +0300

    update

--- R/preprocess.R
+++ R/preprocess.R
@@ -3,7 +3,18 @@
 }
 
 social_mobility <- read_csv("data/H-2018-Data.csv")
+social_mobility %>% select(SemelEretzEm_C) %>% View()
 
 
 social_mobility %>% 
-  mutate_all(~na_if(., 999999)) %>% View()
+  mutate_all(~na_if(., 999999)) %>%
+  select(-c(shana,
+            contains("Kodem"),
+            SibaMaavarMegurim,
+            contains("Zara"),
+            ))
+  
+  View()
+  
+  
+  
